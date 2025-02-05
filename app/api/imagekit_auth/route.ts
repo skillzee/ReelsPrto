f8aee6cc712f38a3afb77ec8bd1f683710926fee
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-02-05 16:20:54 +0530

    imagekit implementation

---
+++ app/api/imagekit_auth/route.ts
@@ -0,0 +1,24 @@
+import ImageKit from "imagekit"
+import { NextResponse } from "next/server";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+});
+
+export async function GET() {
+
+    try {
+
+        const authParameters = imagekit.getAuthenticationParameters()
+        return NextResponse.json(authParameters);
+        
+    } catch (error) {
+        return NextResponse.json(
+            {error: "Image kit auth failed"},
+            {status: 500}
+        )
+    }
+    
+}
